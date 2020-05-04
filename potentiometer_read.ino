69ac2f6bd2ccc5e20071785fc4abb90a6c37e898
Nrxszv0 <58677365+Nrxszv0@users.noreply.github.com>
2020-05-04 00:14:23 -0500

    Update potentiometer_read.ino

--- potentiometer_read.ino
+++ potentiometer_read.ino
@@ -1,13 +1,17 @@
-int voltPin=A2, readVal;
+int voltPin=A2, readVal, dly=100;
 float v2;
 
 
 void setup() {
   // put your setup code here, to run once:
-
+Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  readVal=analogRead(voltPin);
+  v2=(5./1023.) *readVal;
+  Serial.println(v2);
+  delay(dly);
 
 }
