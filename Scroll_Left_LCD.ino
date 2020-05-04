8530c77532a9b935c366b51f5dfd8e489f2a89fc
Nrxszv0 <58677365+Nrxszv0@users.noreply.github.com>
2020-05-04 16:49:55 -0500

    Add files via upload

---
+++ Scroll_Left_LCD.ino
@@ -0,0 +1,21 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+void setup() {
+  // put your setup code here, to run once:
+  lcd.begin(16, 2);
+  lcd.print("Hello");
+  lcd.setCursor(0, 1);
+  lcd.print("World");
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  for (int posCounter = 0; posCounter < 16; posCounter++) {
+    lcd.scrollDisplayLeft();
+    delay(150);
+  }
+
+}
