9be60b4125a99e75ecca11e05f244628f4bdb9f6
Nrxszv0 <58677365+Nrxszv0@users.noreply.github.com>
2020-05-04 00:52:25 -0500

    Update Bright_Dim_LED.ino
    Added Serial.println(anlgVal); to see the value in Serial.

--- Bright_Dim_LED.ino
+++ Bright_Dim_LED.ino
@@ -3,15 +3,19 @@ int ledPin = 11, anlgVal, bdly = 250, dly = 1;
 void setup() {
   // put your setup code here, to run once:
   pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
+  
   for (anlgVal = 0; anlgVal < 255; anlgVal++) {
     analogWrite(ledPin, anlgVal);
+    Serial.println(anlgVal);
     delay(20);
   }
   for (anlgVal = 255; anlgVal > 0; anlgVal--) {
     analogWrite(ledPin, anlgVal);
+    Serial.println(anlgVal);
     delay(20);
   }
 }
