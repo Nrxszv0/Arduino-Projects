50c021c725a3905bc671e4cee6d548af5d682125
Nrxszv0 <58677365+Nrxszv0@users.noreply.github.com>
2020-05-07 22:43:21 -0500

    Update Keyboard_Piezo.ino

--- Keyboard_Piezo.ino
+++ Keyboard_Piezo.ino
@@ -1,23 +1,44 @@
-int piezPin = 13, toneVal = 0, noteLen = 10, dly = 250, b1Pin = 6, b2Pin = 7, b3Pin = 8, b4Pin = 9, b5Pin = 10, b6Pin = 11, b7Pin = 12;
+int piezPin = 13, noteLen = 10, dly = 50, b0Pin = 6, b1Pin = 7, b2Pin = 8, b3Pin = 9, b4Pin = 10, b5Pin = 11, b6Pin = 12, b0St = 0, b1St = 0, b2St = 0, b3St = 0, b4St = 0, b5St = 0, b6St = 0;
 int notes[] = {261, 277, 294, 311, 330, 349, 370, 392, 415, 440};
 //            mid C  C#   D    D#   E    F    F#   G    G#   A
+String b0S = "Button 0 was pressed", b1S = "Button 1 was pressed", b2S = "Button 2 was pressed", b3S = "Button 3 was pressed", b4S = "Button 4 was pressed", b5S = "Button 5 was pressed", b6S = "Button 6 was pressed";
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  pinMode(b0Pin, INPUT);
   pinMode(b1Pin, INPUT);
   pinMode(b2Pin, INPUT);
   pinMode(b3Pin, INPUT);
   pinMode(b4Pin, INPUT);
   pinMode(b5Pin, INPUT);
   pinMode(b6Pin, INPUT);
-  pinMode(b7Pin, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  for (int i = 0; i < noteLen; i++) {
-    tone(piezPin, notes[i]);
-    delay(dly);
+  b0St = digitalRead(b0Pin);
+  b1St = digitalRead(b1Pin);
+  b2St = digitalRead(b2Pin);
+  b3St = digitalRead(b3Pin);
+  b4St = digitalRead(b4Pin);
+  b5St = digitalRead(b5Pin);
+  b6St = digitalRead(b6Pin);
+  //  for (int i = 0; i < noteLen; i++) {
+  //    tone(piezPin, notes[i]);
+  //    delay(dly);
+  //  }
+
+  if (b0St == HIGH) {
+    tone(piezPin, notes[0]);
+    Serial.print(b0S);    
+    
   }
-  noTone(piezPin);
+  else {
+    noTone(piezPin);
+  }
+
+
+
+
+
 }
