5d2828d39a5696fe5d8cef6263b705c5f784e52f
Nrxszv0 <58677365+Nrxszv0@users.noreply.github.com>
2020-05-07 22:48:27 -0500

    Update Keyboard_Piezo.ino
    Increased the delay to 100 milliseconds from 50 milliseconds.

--- Keyboard_Piezo.ino
+++ Keyboard_Piezo.ino
@@ -1,4 +1,4 @@
-int piezPin = 13, noteLen = 10, dly = 50, b0Pin = 6, b1Pin = 7, b2Pin = 8, b3Pin = 9, b4Pin = 10, b5Pin = 11, b6Pin = 12, b0St = 0, b1St = 0, b2St = 0, b3St = 0, b4St = 0, b5St = 0, b6St = 0;
+int piezPin = 13, noteLen = 10, dly = 100, b0Pin = 6, b1Pin = 7, b2Pin = 8, b3Pin = 9, b4Pin = 10, b5Pin = 11, b6Pin = 12, b0St = 0, b1St = 0, b2St = 0, b3St = 0, b4St = 0, b5St = 0, b6St = 0;
 int notes[] = {261, 277, 294, 311, 330, 349, 370, 392, 415, 440};
 //            mid C  C#   D    D#   E    F    F#   G    G#   A
 String b0S = "Button 0 was pressed", b1S = "Button 1 was pressed", b2S = "Button 2 was pressed", b3S = "Button 3 was pressed", b4S = "Button 4 was pressed", b5S = "Button 5 was pressed", b6S = "Button 6 was pressed";
@@ -41,13 +41,13 @@ void loop() {
     delay(dly);
   }
 
-   else if (b3St == HIGH) {
+  else if (b3St == HIGH) {
     tone(piezPin, notes[3]);
     Serial.println(b3S);
     delay(dly);
   }
 
-   else if (b4St == HIGH) {
+  else if (b4St == HIGH) {
     tone(piezPin, notes[4]);
     Serial.println(b4S);
     delay(dly);
@@ -64,7 +64,7 @@ void loop() {
     Serial.println(b6S);
     delay(dly);
   }
-    
+
   else {
     noTone(piezPin);
   }
