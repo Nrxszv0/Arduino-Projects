26196f02ae582002d61b20668be5b6b6e9e9d0ba
Nrxszv0 <58677365+Nrxszv0@users.noreply.github.com>
2020-05-09 00:22:15 -0500

    Add files via upload

---
+++ Tone_Melody.ino
@@ -0,0 +1,25 @@
+#include "pitches.h"
+int piezoPin = 13;
+int notes[] = {
+  NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4
+};
+int noteLengths[] = {
+  4, 8, 8, 4, 4, 4, 4, 4
+};
+void setup() {
+  // put your setup code here, to run once:
+  for (int currentNote = 0; currentNote < 8; currentNote++) {
+    int noteLength = 1000 / noteLengths[currentNote];
+    tone(piezoPin, notes[currentNote], noteLength);
+    int notesPause = noteLength * 1.30;
+    delay(notesPause);
+    noTone(piezoPin);
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //  tone(piezoPin, notes[0], 20);
+  //  delay(1000);
+
+}
