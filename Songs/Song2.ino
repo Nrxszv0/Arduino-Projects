dfeb4f9c4442e05152ed4abdfec067c26033785f
Nrxszv0 <58677365+Nrxszv0@users.noreply.github.com>
2020-05-09 00:12:47 -0500

    Add files via upload
    Uses the pitches librara

---
+++ Songs/Song2.ino
@@ -0,0 +1,16 @@
+#include "pitches.h"
+int piezoPin = 13;
+int notes[] = {
+  NOTE_A4
+};
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+//  tone(piezoPin, notes[0], 20);
+
+}
