e1ab27cf1cab8877c96bc7a5211e5d3a99a1980e
Nrxszv0 <58677365+Nrxszv0@users.noreply.github.com>
2020-05-11 22:28:49 -0500

    Update Choose_RGB_Value_Serial.ino
    Added a serial line that prints out the value of the LED.

--- Choose_RGB_Value_Serial.ino
+++ Choose_RGB_Value_Serial.ino
@@ -1,5 +1,5 @@
 int rPin = 11, bPin = 12, gPin = 13, rVal, gVal, bVal;
-String msgR = "What red value do you want? ", msgG = "What green value do you want? ", msgB = "What blue value do you want? ";
+String msgR = "What red value do you want? ", msgG = "What green value do you want? ", msgB = "What blue value do you want? ", msgLED = "The value of the LED is: ";
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -32,4 +32,10 @@ void loop() {
   analogWrite(rPin, rVal);
   analogWrite(gPin, gVal);
   analogWrite(bPin, bVal);
+  Serial.print(msgLED);
+  Serial.print(rVal);
+  Serial.print(", ");
+  Serial.print(gVal);
+  Serial.print(", ");
+  Serial.println(bVal);
 }
