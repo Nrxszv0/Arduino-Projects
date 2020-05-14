4cf3261c4bf67982c3eb3f23ebe9e405e6b89fe1
Nrxszv0 <58677365+Nrxszv0@users.noreply.github.com>
2020-05-14 18:09:55 -0500

    Update Servo_Val_Loop.ino
    Added ability to change the delay and increment in the Serial.

--- Servo_Val_Loop.ino
+++ Servo_Val_Loop.ino
@@ -1,26 +1,41 @@
 #include <Servo.h>
-int servoPin = 13, serVal, dly=1, increment=10;
+int servoPin = 13, serVal, dly = 100, increment = 40;
+boolean t = true;
 Servo ser;
-String serMsg="The degree of the servo is: ";
+String serMsg = "The degree of the servo is: ", msg = "What do you want the delay to be?" ,msg2 = "What do you want the increment to be?";
 void setup() {
   // put your setup code here, to run once:
-Serial.begin(9600);
-ser.attach(servoPin);
+  Serial.begin(9600);
+  ser.attach(servoPin);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  for(int i=0; i<=160; i=i+increment) {
-    ser.write(i);
-    Serial.print(serMsg);
-    Serial.println(i);
-    delay(dly); 
-  }
+  Serial.println(msg);
+  while (Serial.available() == 0) {
 
-  for(int i=160; i>=0; i=i-increment) {
-    ser.write(i);
-    Serial.print(serMsg);
-    Serial.println(i);
-    delay(dly); 
+  }
+  dly = Serial.parseInt();
+
+  Serial.println(msg2);
+  while (Serial.available() == 0) {
+
+  }
+  increment = Serial.parseInt();
+  while (t) {
+    for (int i = 0; i <= 160; i = i + increment) {
+
+      ser.write(i);
+      Serial.print(serMsg);
+      Serial.println(i);
+      delay(dly);
+    }
+
+    for (int i = 160; i >= 0; i = i - increment) {
+      ser.write(i);
+      Serial.print(serMsg);
+      Serial.println(i);
+      delay(dly);
+    }
   }
 }
