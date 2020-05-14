1ef6ac96df1e36c727cf2cae908dc54deade7cae
Nrxszv0 <58677365+Nrxszv0@users.noreply.github.com>
2020-05-13 23:21:01 -0500

    Update Number_Of_Beeps_Serial.ino
    Added ability to change the delay in between beeps.

--- Number_Of_Beeps_Serial.ino
+++ Number_Of_Beeps_Serial.ino
@@ -1,5 +1,5 @@
 int piezoPin = 13, dly = 300, beepsVal, piezoVal = 500, len = 250;
-String msgB = "Enter the number of piezo beeps: " , msgPV = "Enter the value of the piezo tone: ", msgL = "Enter the time for the piezo to create sound (in milliseconds): " ;
+String msgB = "Enter the number of piezo beeps: " , msgPV = "Enter the value of the piezo tone: ", msgL = "Enter the time for the piezo to create sound (in milliseconds): ", msgD = "Enter the time for the delay inbetween the beeps (in milliseconds): ";
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -25,11 +25,15 @@ void loop() {
 
   }
   len = Serial.parseInt();
+
+  Serial.println(msgD);
+  while (Serial.available() == 0) {
+
+  }
+  dly = Serial.parseInt();
   for (int i = 0; i < beepsVal; i++) {
     tone(piezoPin, piezoVal, len);
     delay(dly);
     noTone(piezoPin);
   }
-
-
 }
