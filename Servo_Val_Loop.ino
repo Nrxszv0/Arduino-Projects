8565f105ebc6facfe805c0575b31c1863b8a0ca6
Nrxszv0 <58677365+Nrxszv0@users.noreply.github.com>
2020-05-14 17:55:09 -0500

    Add files via upload

---
+++ Servo_Val_Loop.ino
@@ -0,0 +1,16 @@
+#include <Servo.h>
+int servoPin = 13, serVal, dly=100;
+Servo ser;
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+ser.attach(servoPin);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for(int i=0; i<=160; i++) {
+    ser.write(i);
+    delay(dly); 
+  }
+}
