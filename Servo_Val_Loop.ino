d21847666aad8e923b9d0974906f46180fbba862
Nrxszv0 <58677365+Nrxszv0@users.noreply.github.com>
2020-05-14 17:56:55 -0500

    Update Servo_Val_Loop.ino
    Decreased delay and added a loop for returning to 0;

--- Servo_Val_Loop.ino
+++ Servo_Val_Loop.ino
@@ -1,5 +1,5 @@
 #include <Servo.h>
-int servoPin = 13, serVal, dly=100;
+int servoPin = 13, serVal, dly=10;
 Servo ser;
 void setup() {
   // put your setup code here, to run once:
@@ -10,6 +10,11 @@ ser.attach(servoPin);
 void loop() {
   // put your main code here, to run repeatedly:
   for(int i=0; i<=160; i++) {
+    ser.write(i);
+    delay(dly); 
+  }
+
+  for(int i=160; i>=0; i--) {
     ser.write(i);
     delay(dly); 
   }
