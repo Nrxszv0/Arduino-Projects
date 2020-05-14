f5572a64efdd48e3eb8faed6b24057d018f60951
Nrxszv0 <58677365+Nrxszv0@users.noreply.github.com>
2020-05-13 23:11:31 -0500

    Update Number_Of_Beeps_Serial.ino
    Added ability to change the value of the tone.

--- Number_Of_Beeps_Serial.ino
+++ Number_Of_Beeps_Serial.ino
@@ -1,23 +1,29 @@
-int piezoPin=13, dly=300, beepsVal, piezoVal=500, len=250; 
-String msg="Enter the number of piezo beeps: ";
+int piezoPin = 13, dly = 300, beepsVal, piezoVal = 500, len = 250;
+String msgB = "Enter the number of piezo beeps: " , msgPV = "Enter the value of the piezo tone: ";
 void setup() {
   // put your setup code here, to run once:
-Serial.begin(9600);
-pinMode(piezoPin, OUTPUT);
+  Serial.begin(9600);
+  pinMode(piezoPin, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-Serial.println(msg);
-while(Serial.available()==0) {
-  
-}
-beepsVal=Serial.parseInt();
-for(int i=0; i<beepsVal; i++) {
-  tone(piezoPin, piezoVal, len);
-  delay(dly);
-  noTone(piezoPin);
-}
+  Serial.println(msgB);
+  while (Serial.available() == 0) {
+
+  }
+  beepsVal = Serial.parseInt();
+
+  Serial.println(msgPV);
+  while (Serial.available() == 0) {
+
+  }
+  piezoVal = Serial.parseInt();
+  for (int i = 0; i < beepsVal; i++) {
+    tone(piezoPin, piezoVal, len);
+    delay(dly);
+    noTone(piezoPin);
+  }
 
 
 }
