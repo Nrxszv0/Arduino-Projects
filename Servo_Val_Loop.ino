7efa243084a4d2dfe5e378a1cdc7abf913eaffbb
Nrxszv0 <58677365+Nrxszv0@users.noreply.github.com>
2020-05-14 18:03:55 -0500

    Update Servo_Val_Loop.ino
    Added increment

--- Servo_Val_Loop.ino
+++ Servo_Val_Loop.ino
@@ -1,5 +1,5 @@
 #include <Servo.h>
-int servoPin = 13, serVal, dly=1;
+int servoPin = 13, serVal, dly=1, increment=10;
 Servo ser;
 String serMsg="The degree of the servo is: ";
 void setup() {
@@ -10,14 +10,14 @@ ser.attach(servoPin);
 
 void loop() {
   // put your main code here, to run repeatedly:
-  for(int i=0; i<=160; i++) {
+  for(int i=0; i<=160; i=i+increment) {
     ser.write(i);
     Serial.print(serMsg);
     Serial.println(i);
     delay(dly); 
   }
 
-  for(int i=160; i>=0; i--) {
+  for(int i=160; i>=0; i=i-increment) {
     ser.write(i);
     Serial.print(serMsg);
     Serial.println(i);
