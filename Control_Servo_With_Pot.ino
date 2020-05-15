4841dc0d466bb8cf1b8ac948b794c165181440df
Nrxszv0 <58677365+Nrxszv0@users.noreply.github.com>
2020-05-15 17:09:42 -0500

    Update Control_Servo_With_Pot.ino
    Added ability to change the maximum angle and the delay form the Serial.

--- Control_Servo_With_Pot.ino
+++ Control_Servo_With_Pot.ino
@@ -1,21 +1,37 @@
 #include <Servo.h>
-int serPin = 13, serAng, potPin=A0, potVal, dly=10;
+int serPin = 13, serAng, potPin = A0, potVal, dly = 5 , maxAng;
+bool t = true;
 Servo ser;
-String msgPV="Potentiometer Value: ", msgSA=", Servo Angle: ";
+String msgPV = "Potentiometer Value: ", msgSA = ", Servo Angle: ", msgDly = "What do you want the delay to be?", msgMxAng = "What do you want the maximum servo angle to be?";
 void setup() {
   // put your setup code here, to run once:
-Serial.begin(9600);
-ser.attach(serPin);
+  Serial.begin(9600);
+  ser.attach(serPin);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  potVal=analogRead(potPin);
-  Serial.print(msgPV);
-  Serial.print(potVal);
-  serAng=map(potVal,0,1023,0,165);
-  Serial.print(msgSA);
-  Serial.println(serAng);
-  ser.write(serAng);
-  delay(dly);
+
+  Serial.println(msgDly);
+  while (Serial.available() == 0) {
+
+  }
+  dly = Serial.parseInt();
+
+  Serial.println(msgMxAng);
+  while (Serial.available() == 0) {
+
+  }
+  maxAng = Serial.parseInt();
+  while (t) {
+    potVal = analogRead(potPin);
+    Serial.print(msgPV);
+    Serial.print(potVal);
+    serAng = map(potVal, 0, 1023, 0, maxAng);
+    Serial.print(msgSA);
+    Serial.println(serAng);
+    ser.write(serAng);
+    delay(dly);
+  }
+
 }
