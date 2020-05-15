a334bd077f48f1566be261bab968131167e0b8a3
Nrxszv0 <58677365+Nrxszv0@users.noreply.github.com>
2020-05-14 19:09:38 -0500

    Update Servo_Val_No_For_Loop.ino
    Added ability to change the delay, maximum value, and the minimum value.

--- Servo_Val_No_For_Loop.ino
+++ Servo_Val_No_For_Loop.ino
@@ -1,7 +1,8 @@
 #include <Servo.h>
 int servoPin = 13, serVal, dly = 500, maxVal = 160, minVal = 0;
+bool t=true;
 Servo ser;
-String serMsg = "The degree of the servo is: ";
+String serMsg = "The degree of the servo is: " , msgD = "What should the delay be?", msgMx = "What should the maximum value be?", msgMn = "What should the minimum value be?";
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -9,10 +10,27 @@ void setup() {
 }
 
 void loop() {
-  ser.write(minVal);
-  delay(dly);
-  ser.write(maxVal);
-  delay(dly);
+  Serial.println(msgD);
+  while (Serial.available() == 0) {
 
+  }
+  dly = Serial.parseInt();
 
+  Serial.println(msgMn);
+  while (Serial.available() == 0) {
+
+  }
+  minVal = Serial.parseInt();
+
+  Serial.println(msgMx);
+  while (Serial.available() == 0) {
+
+  }
+  maxVal = Serial.parseInt();
+  while (t) {
+    ser.write(minVal);
+    delay(dly);
+    ser.write(maxVal);
+    delay(dly);
+  }
 }
