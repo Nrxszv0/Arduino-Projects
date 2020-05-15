5ed61471270c5871d0cf27ab39c3ed3d3afb2efb
Nrxszv0 <58677365+Nrxszv0@users.noreply.github.com>
2020-05-15 15:46:58 -0500

    Update Control_Servo_With_Pot.ino

--- Control_Servo_With_Pot.ino
+++ Control_Servo_With_Pot.ino
@@ -1,7 +1,7 @@
 #include <Servo.h>
 int serPin = 13, serAng, potPin=A0, potVal, dly=10;
 Servo ser;
-
+String msgPV="Potentiometer Value: ", msgSA=", Servo Angle: ",
 void setup() {
   // put your setup code here, to run once:
 Serial.begin(9600);
@@ -11,9 +11,11 @@ ser.attach(serPin);
 void loop() {
   // put your main code here, to run repeatedly:
   potVal=analogRead(potPin);
+  Serial.print(msgPV);
+  Serial.print(potVal);
   serAng=map(potVal,0,1023,0,165);
+  Serial.print(msgSA);
+  Serial.println(serAng);
   ser.write(serAng);
   delay(dly);
-  
-
 }
