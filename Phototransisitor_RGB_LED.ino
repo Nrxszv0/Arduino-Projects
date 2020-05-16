02af6da1b464f84389f4b3d3503748c0d1410e15
Nrxszv0 <58677365+Nrxszv0@users.noreply.github.com>
2020-05-16 01:26:08 -0500

    Add files via upload
    I'm just getting started. I am going to sleep now. I will finish it later today. I wired up the hardware and now I have to do the software.

---
+++ Phototransisitor_RGB_LED.ino
@@ -0,0 +1,11 @@
+int redPhotoPin=A0, greenPhotoPin=A1, bluePhotoPin=A2, redLedPin=9, greenLedPin=10, blueLedPin=11, redPhotoVal, redLedVal, greenPhotoVal, greenLedVal, bluePhotoVal, blueLedVal;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
