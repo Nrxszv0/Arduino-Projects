bbe16c4bd4157763df99720bf20c61772e7e228e
Nrxszv0 <58677365+Nrxszv0@users.noreply.github.com>
2020-05-16 23:59:44 -0500

    Update Piezo_Theremin.ino
    Kinda fix

--- Piezo_Theremin.ino
+++ Piezo_Theremin.ino
@@ -1,9 +1,11 @@
 int piezoPin, piezoVal, sensVal, sensHigh = 0, sensLow = 1023, tinyLedPin = 13, sensPin = A0, pitch, maxPitch = 4978, minPitch = 31, duration = 20, dly=10;
-
+String sensST="Sensor Value: ", pitchST=", Pitch Value: ", serMessage="Please Wait\nThe sensor is calibrating";
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
   pinMode(tinyLedPin, OUTPUT);
   digitalWrite(tinyLedPin, HIGH);
+  Serial.println(serMessage);
   while (millis() < 5000) {
     sensVal = analogRead(sensPin);
   }
@@ -20,6 +22,10 @@ void loop() {
   // put your main code here, to run repeatedly:
   sensVal=analogRead(sensPin);
   pitch = map(sensVal, sensLow, sensHigh, minPitch, maxPitch);
+  Serial.print(sensST);
+  Serial.print(sensVal);
+  Serial.print(pitchST);
+  Serial.println(pitch);
   tone(piezoPin, pitch, duration);
   delay(dly);
 }
