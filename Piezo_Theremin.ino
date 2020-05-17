82502a2078e61f2022d881d4d6d0ab2f7b102789
Nrxszv0 <58677365+Nrxszv0@users.noreply.github.com>
2020-05-17 00:11:38 -0500

    Update Piezo_Theremin.ino
    It still doesn't work. I'll look fix later today.

--- Piezo_Theremin.ino
+++ Piezo_Theremin.ino
@@ -1,5 +1,5 @@
-int piezoPin, piezoVal, sensVal, sensHigh = 0, sensLow = 1023, tinyLedPin = 13, sensPin = A0, pitch, maxPitch = 4978, minPitch = 31, duration = 20, dly=10;
-String sensST="Sensor Value: ", pitchST=", Pitch Value: ", serMessage="Please Wait\nThe sensor is calibrating";
+int piezoPin = 10, piezoVal, sensVal, sensHigh = 0, sensLow = 1023, tinyLedPin = 13, sensPin = A0, pitch, maxPitch = 4978, minPitch = 31, duration = 20, dly = 10;
+String sensST = "Sensor Value: ", pitchST = ", Pitch Value: ", serMessage = "Please Wait\nThe sensor is calibrating", lowST = "Sensor Low: ", highST = ", Sensor High: ";
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -8,20 +8,27 @@ void setup() {
   Serial.println(serMessage);
   while (millis() < 5000) {
     sensVal = analogRead(sensPin);
+    if (sensVal > sensHigh) {
+      sensHigh = sensVal;
+    }
+    if (sensVal < sensLow) {
+      sensLow = sensVal;
+    }
   }
-  if (sensVal > sensHigh) {
-    sensHigh = sensVal;
-  }
-  if (sensVal < sensLow) {
-    sensLow = sensVal;
-  }
+
   digitalWrite(tinyLedPin, LOW);
+  Serial.print(sensST);
+  Serial.print(sensLow);
+  Serial.print(highST);
+  Serial.print(sensHigh);
+  Serial.print("\n");
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  sensVal=analogRead(sensPin);
-  pitch = map(sensVal, sensLow, sensHigh, minPitch, maxPitch);
+  sensVal = analogRead(sensPin);
+  pitch = map(sensVal, sensLow, sensHigh, 50, 4000);
   Serial.print(sensST);
   Serial.print(sensVal);
   Serial.print(pitchST);
