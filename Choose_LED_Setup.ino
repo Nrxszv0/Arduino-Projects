ac05db4675c7ea8e8812eac813a56d4c1a6ad804
Nrxszv0 <58677365+Nrxszv0@users.noreply.github.com>
2020-05-22 00:17:25 -0500

    Update Choose_LED_Setup.ino
    I added string variables for the statements.

--- Choose_LED_Setup.ino
+++ Choose_LED_Setup.ino
@@ -1,5 +1,5 @@
-int rLED=11, rVal, yLED=12, yVal, gLED=13,gVal;
-String msg="Do you want to turn on the Red, Yellow, or Green LED?", ans;
+int rLED = 11, rVal, yLED = 12, yVal, gLED = 13, gVal;
+String msg = "Do you want to turn on the Red, Yellow, or Green LED?", ans, r = "red", y = "yellow" , g = "green", n = "none";
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -7,37 +7,37 @@ void setup() {
   pinMode(yLED, OUTPUT);
   pinMode(gLED, OUTPUT);
   Serial.println(msg);
-  while(Serial.available() == 0) {
-    
+  while (Serial.available() == 0) {
+
   }
-  ans=Serial.readString();
+  ans = Serial.readString();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   Serial.println(ans);
-  if(ans == "red") {
+  if (ans == r) {
     digitalWrite(rLED, HIGH);
   }
-  
-  else if(ans =="yellow") {
+
+  else if (ans == y) {
     digitalWrite(yLED, HIGH);
   }
 
-  else if(ans =="green") {
+  else if (ans == g) {
     digitalWrite(gLED, HIGH);
   }
-  else if(ans =="none") {
+  else if (ans == n) {
     digitalWrite(rLED, LOW);
     digitalWrite(yLED, LOW);
     digitalWrite(gLED, LOW);
   }
- 
-  if(Serial.available() != 0) {
+
+  if (Serial.available() != 0) {
     Serial.println(msg);
-  while(Serial.available() == 0) {
-    
-  }
-  ans=Serial.readString();
+    while (Serial.available() == 0) {
+
+    }
+    ans = Serial.readString();
   }
 }
