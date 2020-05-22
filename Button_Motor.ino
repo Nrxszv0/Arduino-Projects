1300d84a1392e83675392bc424656d4b46fcdfc6
Nrxszv0 <58677365+Nrxszv0@users.noreply.github.com>
2020-05-21 22:36:33 -0500

    Update Button_Motor.ino
    Added messages that display the state of the motors.

--- Button_Motor.ino
+++ Button_Motor.ino
@@ -1,10 +1,10 @@
 int buttonPin = 13, motorPin = 2, buttonState = 0, buttonPin2 = 12, motorPin2 = 3, buttonState2 = 0;
-
+String b1 = "Button 1 State: ", b2 = ", Button 2 State: ";
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(motorPin, OUTPUT);
-  pinMode(motorPin,OUTPUT);
+  pinMode(motorPin, OUTPUT);
   pinMode(buttonPin, INPUT);
   pinMode(buttonPin, INPUT);
 }
@@ -12,13 +12,15 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   buttonState = digitalRead(buttonPin);
-  buttonState2= digitalRead(buttonPin2);
-  Serial.println(buttonState);
+  buttonState2 = digitalRead(buttonPin2);
+  Serial.print(b1);
+  Serial.print(buttonState);
+  Serial.print(b2);
   Serial.println(buttonState2);
   if (buttonState == HIGH) {
     digitalWrite(motorPin, HIGH);
   }
-  else if(buttonState2==HIGH) {
+  else if (buttonState2 == HIGH) {
     digitalWrite(motorPin2, HIGH);
   }
   else {
