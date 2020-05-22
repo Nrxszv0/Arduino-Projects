a075b5a6c8eabacce36f87cbf8c6425c29636b9b
Nrxszv0 <58677365+Nrxszv0@users.noreply.github.com>
2020-05-21 22:03:36 -0500

    Update Button_Motor.ino
    Added a second Motor

--- Button_Motor.ino
+++ Button_Motor.ino
@@ -1,18 +1,28 @@
-int buttonPin=2, motorPin=10, buttonState=0;
+int buttonPin = 13, motorPin = 2, buttonState = 0, buttonPin2 = 12, motorPin2 = 3, buttonState2 = 0;
 
 void setup() {
   // put your setup code here, to run once:
-pinMode(motorPin, OUTPUT);
-pinMode(buttonPin,INPUT);
+  Serial.begin(9600);
+  pinMode(motorPin, OUTPUT);
+  pinMode(motorPin,OUTPUT);
+  pinMode(buttonPin, INPUT);
+  pinMode(buttonPin, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-buttonState=digitalRead(buttonPin);
-if(buttonState== HIGH){
-  digitalWrite(motorPin, HIGH);
-}
-else{
-  digitalWrite(motorPin, LOW);
-}
+  buttonState = digitalRead(buttonPin);
+  buttonState2= digitalRead(buttonPin2);
+  Serial.println(buttonState);
+  Serial.println(buttonState2);
+  if (buttonState == HIGH) {
+    digitalWrite(motorPin, HIGH);
+  }
+  else if(buttonState2==HIGH) {
+    digitalWrite(motorPin2, HIGH);
+  }
+  else {
+    digitalWrite(motorPin, LOW);
+    digitalWrite(motorPin2, LOW);
+  }
 }
