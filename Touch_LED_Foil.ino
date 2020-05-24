2524cd2acf1b34c226bbdd27cfa023c1cf356275
Nrxszv0 <58677365+Nrxszv0@users.noreply.github.com>
2020-05-24 01:37:02 -0500

    Update Touch_LED_Foil.ino
    It doesn't work. The sensor only returns a value of 0

--- Touch_LED_Foil.ino
+++ Touch_LED_Foil.ino
@@ -1,10 +1,24 @@
-int rLED=13;
+#include <CapacitiveSensor.h>
+int rLED = 13, foilThreshold = 1000, sendPin=4, sensePin=2;
+CapacitiveSensor capSens = CapacitiveSensor(sendPin, sensePin);
+long sensVal;
+
 void setup() {
   // put your setup code here, to run once:
-pinMode(rLED, OUTPUT);
+  Serial.begin(9600);
+  pinMode(rLED, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-digitalWrite(rLED, HIGH);
+  digitalWrite(rLED, HIGH);
+  sensVal = capSens.capacitiveSensor(30);
+  Serial.println(sensVal);
+  if (sensVal > foilThreshold) {
+    digitalWrite(rLED, HIGH);
+  }
+  else {
+    digitalWrite(rLED, LOW);
+  }
+  delay(15);
 }
