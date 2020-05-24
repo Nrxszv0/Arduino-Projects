ef085eea2a2f488215bbcb51b59d4f0b460524a2
Nrxszv0 <58677365+Nrxszv0@users.noreply.github.com>
2020-05-24 18:34:40 -0500

    Update Touch_LED_Foil.ino
    I was using the wrong resistor. It works now but the light doesn't turn off. It only gets dimmer. I don't know why.

--- Touch_LED_Foil.ino
+++ Touch_LED_Foil.ino
@@ -1,24 +1,22 @@
 #include <CapacitiveSensor.h>
-int rLED = 13, foilThreshold = 1000, sendPin=4, sensePin=2;
+int rLED = 11, foilThreshold = 1000, sendPin=4, sensePin=2;
 CapacitiveSensor capSens = CapacitiveSensor(sendPin, sensePin);
 long sensVal;
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(rLED, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   digitalWrite(rLED, HIGH);
   sensVal = capSens.capacitiveSensor(30);
   Serial.println(sensVal);
   if (sensVal > foilThreshold) {
     digitalWrite(rLED, HIGH);
   }
-  else {
-    digitalWrite(rLED, LOW);
+  else if( sensVal< foilThreshold){
+    digitalWrite(rLED,LOW);
   }
   delay(15);
 }
