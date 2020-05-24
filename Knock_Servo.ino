2447a685e18218b9a79d127365191bc699d33725
Nrxszv0 <58677365+Nrxszv0@users.noreply.github.com>
2020-05-23 23:28:22 -0500

    Update Knock_Servo.ino
    Testing LEDs

--- Knock_Servo.ino
+++ Knock_Servo.ino
@@ -1,22 +1,26 @@
 #include <Servo.h>
 Servo kServo;
-int servoPin=10, rLED=11, yLED=12, gLED=13, btnPin=2, piezoPin=A0;
+int servoPin = 10, rLED = 11, yLED = 12, gLED = 13, btnPin = 2, piezoPin = A0;
 
 void setup() {
   // put your setup code here, to run once:
-pinMode(rLED, OUTPUT);
-pinMode(yLED, OUTPUT);
-pinMode(gLED, OUTPUT);
-pinMode(piezoPin, INPUT);
-pinMode(btnPin, INPUT);
-kServo.attach(10);
+  pinMode(rLED, OUTPUT);
+  pinMode(yLED, OUTPUT);
+  pinMode(gLED, OUTPUT);
+  pinMode(piezoPin, INPUT);
+  pinMode(btnPin, INPUT);
+  kServo.attach(10);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-//for(int i=0; i<160; i=i+5){
-//  kServo.write(i);
-//  delay(100);
-//}
-kServo.write(0);
+  //for(int i=0; i<160; i=i+5){
+  //  kServo.write(i);
+  //  delay(100);
+  //}
+  kServo.write(0);
+
+  digitalWrite(rLED, HIGH);
+  digitalWrite(gLED, HIGH);
+  digitalWrite(yLED, HIGH);
 }
