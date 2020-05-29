777d63fa6bc6b1451e275d54ec701a91fae7e087
Nrxszv0 <58677365+Nrxszv0@users.noreply.github.com>
2020-05-28 23:06:07 -0500

    Update Programmable_Boat.ino
    Deleted potentiometer and fourth speed button.

--- Programmable_Boat.ino
+++ Programmable_Boat.ino
@@ -1,7 +1,7 @@
 #include <Servo.h>
 int contPin1 = 4, contPin2 = 3, enablePin = 5, directionBtnPin = 12, powerStateBtnPin = 13;
-int motorSpeedBtn1 = 8, motorSpeedBtn2 = 9, motorSpeedBtn3 = 10, motorSpeedBtn4 = 11;
-int speedVal1 = 64, speedVal2 = 128, speedVal3 = 192, speedVal4 = 255;
+int motorSpeedBtn1 = 8, motorSpeedBtn2 = 9, motorSpeedBtn3 = 10;
+int speedVal1 = 64, speedVal2 = 128, speedVal3 = 192;
 int powerBtnState = 0, prevPowerBtnState = 0, directionBtnState = 0, prevDirectionBtnState = 0, motorEnabled = 0, motorSpeed = 0, motorDirection = 1;
 int servoPin = 2, serIncPin=7, serDecPin=8, serAng, serPotVal, maxSerAng = 165, startSerVal = 0, minSerAng = 0;
 Servo servo;
