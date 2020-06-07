f601fb7591e8464d729a52fd2c1eb9883575ab03
Nrxszv0 <58677365+Nrxszv0@users.noreply.github.com>
2020-06-07 01:42:45 -0500

    Add files via upload

---
+++ Photodetector.ino
@@ -0,0 +1,11 @@
+int photoPin = A0, photoVal;
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+photoVal = analogRead(photoPin);
+Serial.println(photoVal);
+}
