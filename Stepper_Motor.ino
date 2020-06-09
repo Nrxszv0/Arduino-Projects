5ef9bbebf6f5a96473678a14067928105931c10b
Nrxszv0 <58677365+Nrxszv0@users.noreply.github.com>
2020-06-09 02:11:06 -0500

    Update Stepper_Motor.ino
    Switch test

--- Stepper_Motor.ino
+++ Stepper_Motor.ino
@@ -1,16 +1,19 @@
 #include <Stepper.h>
 int stepsPerRevolution = 2048, motSpeed = 18 , dly = 200;
+int sPin=7, sState;
 int N1 = 8, N2 = 9, N3 = 10, N4 = 11;
 Stepper stepMotor(stepsPerRevolution, N1, N3, N2, N4);
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   stepMotor.setSpeed(motSpeed);
+  pinMode(sPin ,INPUT);
 }
 
 void loop() {
-  stepMotor.step(stepsPerRevolution);
-  delay(dly);
-  stepMotor.step(-stepsPerRevolution);
-  delay(dly);
+  Serial.println(digitalRead(sPin));
+//  stepMotor.step(stepsPerRevolution);
+//  delay(dly);
+//  stepMotor.step(-stepsPerRevolution);
+//  delay(dly);
 }
