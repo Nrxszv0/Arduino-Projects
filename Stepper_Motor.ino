63984a7c981f69ce595a36e41bdc710176743044
Nrxszv0 <58677365+Nrxszv0@users.noreply.github.com>
2020-06-09 02:32:34 -0500

    Update Stepper_Motor.ino

--- Stepper_Motor.ino
+++ Stepper_Motor.ino
@@ -1,19 +1,39 @@
 #include <Stepper.h>
-int stepsPerRevolution = 2048, motSpeed = 18 , dly = 200;
-int sPin=7, sState;
+int stepsPerRevolution = 2048, motSpeed = 18 , dly = 200, motorDir = 1;
+int sPin = 7, sState=0, prevSState=0;
 int N1 = 8, N2 = 9, N3 = 10, N4 = 11;
 Stepper stepMotor(stepsPerRevolution, N1, N3, N2, N4);
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   stepMotor.setSpeed(motSpeed);
-  pinMode(sPin ,INPUT);
+  pinMode(sPin , INPUT);
 }
 
 void loop() {
-  Serial.println(digitalRead(sPin));
-//  stepMotor.step(stepsPerRevolution);
-//  delay(dly);
-//  stepMotor.step(-stepsPerRevolution);
-//  delay(dly);
+  sState = digitalRead(sPin);
+  if (sState !=prevSState) {
+    if(sState==HIGH){
+      motorDir = !motorDir;      
+    }
+  }
+  if(motorDir == 1) {
+    stepMotor.step(1);
+  }
+  else {
+    stepMotor.step(-1);
+  }
+  
+//  else if(sState ==HIGH){
+//    stepMotor.step(stepsPerRevolution * -motorDir);   
+//    if(sState !=sState) {
+//      break;
+//    }
+//  }
+  Serial.println(motorDir);
+  //  stepMotor.step(stepsPerRevolution);
+  //  delay(dly);
+  //  stepMotor.step(-stepsPerRevolution);
+  //  delay(dly);
+  prevSState = sState;
 }
