791b5afb8bb66f102e2e9d4341faedcacb742304
Nrxszv0 <58677365+Nrxszv0@users.noreply.github.com>
2020-06-09 02:02:25 -0500

    Update Stepper_Motor.ino
    I think 18 is the max motor speed. It is the highest I could get where it would reliably spin

--- Stepper_Motor.ino
+++ Stepper_Motor.ino
@@ -1,10 +1,11 @@
 #include <Stepper.h>
-int stepsPerRevolution=2048, motSpeed=10, dly=200;
-Stepper stepMotor(stepsPerRevolution, 8,10,9,11);
+int stepsPerRevolution = 2048, motSpeed = 18 , dly = 200;
+int N1 = 8, N2 = 9, N3 = 10, N4 = 11;
+Stepper stepMotor(stepsPerRevolution, N1, N3, N2, N4);
 void setup() {
   // put your setup code here, to run once:
-Serial.begin(9600);
-stepMotor.setSpeed(motSpeed);
+  Serial.begin(9600);
+  stepMotor.setSpeed(motSpeed);
 }
 
 void loop() {
