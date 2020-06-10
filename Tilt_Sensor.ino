405f4d3f6eaa204cc216b1856ed205e1efecc05d
Nrxszv0 <58677365+Nrxszv0@users.noreply.github.com>
2020-06-10 12:23:00 -0500

    Update Tilt_Sensor.ino
    Added a second tilt sensor

--- Tilt_Sensor.ino
+++ Tilt_Sensor.ino
@@ -1,12 +1,16 @@
-int tiltPin=2, tiltState;
+int tiltPin=2, tiltState, tiltPin2=2, tiltState2;
 void setup() {
   // put your setup code here, to run once:
 Serial.begin(9600);
 pinMode(tiltPin, INPUT);
+pinMode(tiltPin2, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 tiltState = digitalRead(tiltPin);
-Serial.println(tiltState);
+tiltState2 = digitalRead(tiltPin2);
+Serial.print(tiltState);
+Serial.print("\t\t");
+Serial.println(tiltState2);
 }
