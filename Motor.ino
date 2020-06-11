f9a0a6e69142b0aab96a040889dbf21c1706c848
Nrxszv0 <58677365+Nrxszv0@users.noreply.github.com>
2020-06-10 20:44:17 -0500

    Update Motor.ino

--- Motor.ino
+++ Motor.ino
@@ -1,22 +1,22 @@
-int speedPin=5, dirP1=6, dirP2=7, mSpeed=100;
+int speedPin = 5, dirP1 = 6, dirP2 = 7, mSpeed = 100, boostTime=100, boostSpeed=255;
 void setup() {
   // put your setup code here, to run once:
-Serial.begin(9600);
-pinMode(speedPin, OUTPUT);
-pinMode(dirP1, OUTPUT);
-pinMode(dirP2, OUTPUT);
+  Serial.begin(9600);
+  pinMode(speedPin, OUTPUT);
+  pinMode(dirP1, OUTPUT);
+  pinMode(dirP2, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-digitalWrite(dirP1, HIGH);
-digitalWrite(dirP2, LOW);
-if(millis() <=100){
-  analogWrite(speedPin, 255);
+  digitalWrite(dirP1, HIGH);
+  digitalWrite(dirP2, LOW);
+  if (millis() <= boostTime) {
+    analogWrite(speedPin, boostSpeed);
 
-}
-else{
-  analogWrite(speedPin, mSpeed);
-}
+  }
+  else {
+    analogWrite(speedPin, mSpeed);
+  }
 
 }
