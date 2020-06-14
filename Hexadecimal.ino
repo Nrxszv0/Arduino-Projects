a2516ce8522efe75a6775ceb9bbb58364d3a4276
Nrxszv0 <58677365+Nrxszv0@users.noreply.github.com>
2020-06-13 20:38:27 -0500

    Add files via upload

---
+++ Hexadecimal.ino
@@ -0,0 +1,13 @@
+int dly=500;
+byte myByte=0x0;
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+Serial.println(myByte,HEX);
+myByte = myByte+1;
+delay(dly);
+}
