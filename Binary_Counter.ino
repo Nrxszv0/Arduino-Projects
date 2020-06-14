5f7b83d49044f05ea43be795cb7e3ad1dd87c41a
Nrxszv0 <58677365+Nrxszv0@users.noreply.github.com>
2020-06-14 02:37:27 -0500

    Update Binary_Counter.ino
    Added serial message that shows binary and decimal value

--- Binary_Counter.ino
+++ Binary_Counter.ino
@@ -1,19 +1,23 @@
-int latchPin=11, clockPin=10,dataPin=12;
-int dly=250;
+int latchPin = 11, clockPin = 10, dataPin = 12;
+int dly = 250;
 void setup() {
   // put your setup code here, to run once:
-pinMode(latchPin, OUTPUT);
-pinMode(dataPin, OUTPUT);
-pinMode(clockPin, OUTPUT);
+  Serial.begin(9600);
+  pinMode(latchPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
 }
 
 void loop() {
-  
-for(byte LED =0b00000000; LED<=255; LED++) {
-  digitalWrite(latchPin, LOW);
-shiftOut(dataPin,clockPin,LSBFIRST,LED);
-digitalWrite(latchPin,HIGH);
-delay(dly);
 
-}
+  for (byte LED = 0b00000000; LED <= 255; LED++) {
+    Serial.print(LED, BIN);
+    Serial.print(" = ");
+    Serial.println(LED, DEC);
+    digitalWrite(latchPin, LOW);
+    shiftOut(dataPin, clockPin, LSBFIRST, LED);
+    digitalWrite(latchPin, HIGH);
+    delay(dly);
+
+  }
 }
