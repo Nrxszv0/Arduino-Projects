8ba7f4eb62743733ae12d3f53465a7eebfce8132
Nrxszv0 <58677365+Nrxszv0@users.noreply.github.com>
2020-06-14 01:18:32 -0500

    Update Hexadecimal.ino

--- Hexadecimal.ino
+++ Hexadecimal.ino
@@ -9,8 +9,9 @@ void loop() {
   // put your main code here, to run repeatedly:
 Serial.print(myByte,HEX);
 Serial.print(" = ");
-Serial.println(num);
+Serial.print(myByte,BIN);
+Serial.print(" = ");
+Serial.println(myByte, DEC);
 myByte = myByte+1;
-num = num+1;
 delay(dly);
 }
