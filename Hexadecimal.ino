abd56000027129c982877418be424e2cd6fa677d
Nrxszv0 <58677365+Nrxszv0@users.noreply.github.com>
2020-06-13 20:44:30 -0500

    Update Hexadecimal.ino

--- Hexadecimal.ino
+++ Hexadecimal.ino
@@ -1,5 +1,5 @@
 int dly=500;
-byte myByte=B00001111;
+byte myByte=0xAB;
 void setup() {
   // put your setup code here, to run once:
 Serial.begin(9600);
@@ -7,7 +7,7 @@ Serial.begin(9600);
 
 void loop() {
   // put your main code here, to run repeatedly:
-Serial.println(myByte,HEX);
+Serial.println(myByte,BIN);
 myByte = myByte+1;
 delay(dly);
 }
