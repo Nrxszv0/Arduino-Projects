54127896d1915e5764423242c6be497f23f6e48a
Nrxszv0 <58677365+Nrxszv0@users.noreply.github.com>
2020-06-14 02:50:13 -0500

    Update Binary_Counter.ino
    Added Serial descriptions.

--- Binary_Counter.ino
+++ Binary_Counter.ino
@@ -1,4 +1,4 @@
-int latchPin = 11, clockPin = 10, dataPin = 12;
+int latchPin = 11, clockPin = 10, dataPin = 12, maxLED=255;
 int dly = 250;
 void setup() {
   // put your setup code here, to run once:
@@ -10,16 +10,18 @@ void setup() {
 
 void loop() {
 
-  for (byte LED = 0b00000000; LED <= 255; LED++) {
-    Serial.print(LED, BIN);
-    Serial.print(" = ");
+  for (byte LED = 0b00000000; LED <= maxLED; LED++) {
+    Serial.print("BIN: ");
+    Serial.print(LED, BIN);  
+    Serial.print(" = ");    
+    Serial.print("HEX: ");
     Serial.print(LED, HEX);
     Serial.print(" = ");
+    Serial.print("DEC: ");
     Serial.println(LED, DEC);
     digitalWrite(latchPin, LOW);
     shiftOut(dataPin, clockPin, LSBFIRST, LED);
     digitalWrite(latchPin, HIGH);
     delay(dly);
-
   }
 }
