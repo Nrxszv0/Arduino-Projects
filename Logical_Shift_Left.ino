9ef3d84b85fea65f07b3e8e174e43cd4dde3c856
Nrxszv0 <58677365+Nrxszv0@users.noreply.github.com>
2020-06-14 03:29:33 -0500

    Add files via upload

---
+++ Logical_Shift_Left.ino
@@ -0,0 +1,26 @@
+int latchPin = 11, clockPin = 10, dataPin = 12, maxLED = 255;
+int dly = 250;
+byte LED = 0b00000001;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(latchPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(latchPin, LOW);
+  shiftOut(dataPin, clockPin, LSBFIRST, LED);
+  digitalWrite(latchPin, HIGH);
+  Serial.print("DEC: ");
+  Serial.println(LED, DEC);
+  delay(dly);
+  LED = LED * 2;
+  digitalWrite(latchPin, LOW);
+  shiftOut(dataPin, clockPin, LSBFIRST, LED);
+  digitalWrite(latchPin, HIGH);
+  Serial.print("DEC: ");
+  Serial.println(LED, DEC);
+  delay(dly);
+}
