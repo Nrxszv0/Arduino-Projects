ed36ad36011d0dd8b2671af71f6ab5d218e724a2
Nrxszv0 <58677365+Nrxszv0@users.noreply.github.com>
2020-06-14 21:02:05 -0500

    Add files via upload

---
+++ Circular_Right_Shift.ino
@@ -0,0 +1,36 @@
+int latchPin = 11, clockPin = 10, dataPin = 12, maxLED = 255;
+int dly = 250;
+byte LEDs[] = {
+  128,
+  64,
+  32,
+  16,
+  8,
+  4,
+  2,
+  1,
+  128
+};
+byte LED = 0b00000001;
+byte sLED;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(latchPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+  sLED = LED;
+}
+
+void loop() {
+  digitalWrite(latchPin, LOW);
+  shiftOut(dataPin, clockPin, LSBFIRST, LED);
+  digitalWrite(latchPin, HIGH);
+  Serial.print("DEC: ");
+  Serial.println(LED, DEC);
+  LED = LED * 2;
+  if (LED < 1 ) {
+    LED = sLED;
+  }
+  delay(dly);
+}
