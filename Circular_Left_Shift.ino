380741e24013f5a339b49a6ae369f828479b2554
Nrxszv0 <58677365+Nrxszv0@users.noreply.github.com>
2020-06-14 20:59:26 -0500

    Update Circular_Left_Shift.ino
    It kind of works. It doesn't really scroll but it does reset.

--- Circular_Left_Shift.ino
+++ Circular_Left_Shift.ino
@@ -1,16 +1,5 @@
 int latchPin = 11, clockPin = 10, dataPin = 12, maxLED = 255;
 int dly = 250;
-//byte LEDs[] = {
-//  0b10000000,
-//  0b01000000,
-//  0b00100000,
-//  0b00010000,
-//  0b00001000,
-//  0b00000100,
-//  0b00000010,
-//  0b00000001,
-//  0b10000000
-//};
 byte LEDs[] = {
   128,
   64,
@@ -22,35 +11,26 @@ byte LEDs[] = {
   1,
   128
 };
-byte LED = 0b10000000;
-byte sLED = 0b10000000;
+byte LED = 0b11000100;
+byte sLED;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(latchPin, OUTPUT);
   pinMode(dataPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
+  sLED = LED;
 }
 
 void loop() {
-  //  for (int i = 0; i <9; i++ ) {
-  //    digitalWrite(latchPin, LOW);
-  //    shiftOut(dataPin, clockPin, LSBFIRST, LEDs[i]);
-  //    digitalWrite(latchPin, HIGH);
-  //    Serial.print("DEC: ");
-  //    Serial.println(LEDs[i], DEC);
-  //    delay(dly);
-  //  }
-
   digitalWrite(latchPin, LOW);
   shiftOut(dataPin, clockPin, LSBFIRST, LED);
   digitalWrite(latchPin, HIGH);
   Serial.print("DEC: ");
   Serial.println(LED, DEC);
-  delay(dly);
   LED = LED / 2;
-  if(LED < 1) {
-    LED = sLED; 
+  if (LED < 1) {
+    LED = sLED;
   }
-
+  delay(dly);
 }
