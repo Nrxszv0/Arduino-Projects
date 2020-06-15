24b55c9ebb06a54d8b65adbc10fc33bf91c31bc4
Nrxszv0 <58677365+Nrxszv0@users.noreply.github.com>
2020-06-14 21:17:34 -0500

    Update Circular_Left_Shift.ino
    I had the shifts swapped.

--- Circular_Left_Shift.ino
+++ Circular_Left_Shift.ino
@@ -11,7 +11,7 @@ byte LEDs[] = {
   1,
   128
 };
-byte LED = 0b11000100;
+byte LED = 0b00000001;
 byte sLED;
 void setup() {
   // put your setup code here, to run once:
@@ -28,7 +28,7 @@ void loop() {
   digitalWrite(latchPin, HIGH);
   Serial.print("DEC: ");
   Serial.println(LED, DEC);
-  LED = (LED*128) +(LED / 2);
+  LED = (LED/128) +(LED * 2);
   
   delay(dly);
 }
