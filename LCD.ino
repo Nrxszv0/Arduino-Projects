f2f8f9c1a291602761b9329533ef7d2c20c25121
Nrxszv0 <58677365+Nrxszv0@users.noreply.github.com>
2020-06-15 23:06:26 -0500

    Add files via upload
    It doesn't print the characters.

---
+++ LCD.ino
@@ -0,0 +1,15 @@
+#include <LiquidCrystal.h>
+int rs = 7, e=8, d4 = 9, d5=10, d6=11, d7=12;
+int columns = 16, rows = 2; 
+LiquidCrystal lcd(rs,e,d4,d5,d6,d7);
+void setup() {
+  // put your setup code here, to run once:
+  lcd.begin(columns, rows);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+lcd.setCursor(0,0);
+lcd.print("what si happhening");
+lcd.setCursor(0,1);
+}
