71957e5543c681e69ec0d33770c4654a6c3b80f3
Nrxszv0 <58677365+Nrxszv0@users.noreply.github.com>
2020-06-18 01:04:56 -0500

    Add files via upload
    It doesn't work perfectly. Its off by like an inch. What matters is that I made it.

---
+++ Distance_Meter.ino
@@ -0,0 +1,31 @@
+int trigPin = 12, echoPin = 13, pingTime;
+int dly = 25, sDly = 10;
+int rateMi = 767, rateFt, rateIn;
+float tim, distance;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(sDly);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(sDly);
+  digitalWrite(trigPin, LOW);
+  pingTime = pulseIn(echoPin, HIGH);
+  Serial.print(pingTime);
+
+  //  rateFt = rateMi * 5280;
+  //  rateIn = rateFt * 12;
+  //  Serial.println(rateIn);
+  tim = .5 * pingTime;
+  distance = (((rateMi * tim) / 5280) / 12);
+  Serial.print("\t\t");
+  Serial.println(distance);
+  delay(dly);
+}
