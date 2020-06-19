d61f1107de0f7d46c11e33fc1da9c948378599ae
Nrxszv0 <58677365+Nrxszv0@users.noreply.github.com>
2020-06-19 01:04:10 -0500

    Update Distance_LCD.ino
    Added variables for seconds and microseconds

--- Distance_LCD.ino
+++ Distance_LCD.ino
@@ -3,7 +3,7 @@ int rs = 7, e = 6, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 int columns = 16, rows = 2;
 int trigPin = 12, echoPin = 13, pingTime;
 int sDly = 10, dly = 250;
-float speedOfSound = 767, ft = 5280, in = 12;
+float speedOfSound = 767, ft = 5280, in = 12, scs=3600, mScs=1000000;
 float targetDistance, pingTravelDistance;
 LiquidCrystal lcd(rs, e, d4, d5, d6, d7);
 void setup() {
@@ -24,7 +24,7 @@ void loop() {
   digitalWrite(trigPin, LOW);
   delayMicroseconds(sDly);
   pingTime = pulseIn(echoPin, HIGH);
-  pingTravelDistance = (pingTime * speedOfSound * ft * in) / (3600. * 1000000.);
+  pingTravelDistance = (pingTime * speedOfSound * ft * in) / (scs * mScs);
   targetDistance = pingTravelDistance / 2.;
   Serial.print("Ping Time: ");
   Serial.print(pingTime);
