041d3000d4f26d3fb107af15d62a0b0ad1c15f59
Nrxszv0 <58677365+Nrxszv0@users.noreply.github.com>
2020-06-19 22:19:06 -0500

    Add files via upload

---
+++ LCD/Distance_Switch_Press_LCD.ino
@@ -0,0 +1,47 @@
+#include <LiquidCrystal.h>
+int rs = 7, e = 6, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+int columns = 16, rows = 2;
+int sPin = 8, sVal;
+int trigPin = 12, echoPin = 13;
+int ping;
+String unit="in";
+float pingTotalDistance, targetDistance, prevTargetDistance;
+float soundSpeed = 767, ftInMi = 5280, inInFt = 12, sInHr = 3600, usInSec = 1000000;
+int dly = 250, sDly = 10;
+LiquidCrystal lcd(rs, e, d4, d5, d6, d7);
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  lcd.begin(16  , 2);
+  lcd.setCursor(0, 0);
+  pinMode(sPin, INPUT);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  sVal = digitalRead(sPin);
+  lcd.print("Press to Measure");
+  lcd.setCursor(0, 1);
+  if (sVal == HIGH) {
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(sDly);
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(sDly);
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(sDly);
+    ping = pulseIn(echoPin, HIGH);
+    pingTotalDistance = (ping * soundSpeed * ftInMi * inInFt) / (sInHr * usInSec);
+    targetDistance = pingTotalDistance / 2;
+    Serial.println(targetDistance);
+    prevTargetDistance = targetDistance;
+  }
+  else {
+    Serial.println("Press the button to measure.");
+    lcd.print(prevTargetDistance);
+    lcd.print(unit); 
+  }
+  delay(dly);
+  lcd.clear();
+}
