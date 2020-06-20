22c159409013a9c92d81de8bf59a91605fa7168b
Nrxszv0 <58677365+Nrxszv0@users.noreply.github.com>
2020-06-19 20:12:14 -0500

    Add files via upload

---
+++ Distance_Switch_LCD.ino
@@ -0,0 +1,11 @@
+int sPin = 8;
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+pinMode(sPin, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+Serial.println(digitalRead(sPin));
+}
