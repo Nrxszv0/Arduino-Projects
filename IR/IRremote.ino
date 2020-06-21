170ca50ffc815999371fb74f70fd3527239793af
Nrxszv0 <58677365+Nrxszv0@users.noreply.github.com>
2020-06-21 18:22:59 -0500

    Update IRremote.ino
    Added hex value

--- IR/IRremote.ino
+++ IR/IRremote.ino
@@ -14,92 +14,99 @@ void loop() {
   // put your main code here, to run repeatedly:
   while (IR.decode(&cmd) == 0) {
 
-  }
+  }  
   delay(dly);
+  Serial.print(cmd.value, HEX);
+  Serial.print("==");
   IR.resume();
   if (cmd.value == 0xFFA25D) {
     command = "pwr";
     Serial.println(command);
   }
-  if (cmd.value == 0xFF629D) {
+  else if (cmd.value == 0xFF629D) {
     command = "vol+";
     Serial.println(command);
   }
-  if (cmd.value == 0xFFE21D) {
+  else if (cmd.value == 0xFFE21D) {
     command = "stop";
     Serial.println(command);
   }
-  if (cmd.value == 0xFF22DD) {
+  else if (cmd.value == 0xFF22DD) {
     command = "rew";
     Serial.println(command);
   }
-  if (cmd.value == 0xFF02FD) {
+  else if (cmd.value == 0xFF02FD) {
     command = "pause";
     Serial.println(command);
   }
-  if (cmd.value == 0xFFC23D) {
+  else if (cmd.value == 0xFFC23D) {
     command = "FF";
     Serial.println(command);
   }
-  if (cmd.value == 0xFFE01F) {
+  else if (cmd.value == 0xFFE01F) {
     command = "down";
     Serial.println(command);
   }
-  if (cmd.value == 0xFFA857) {
+  else if (cmd.value == 0xFFA857) {
     command = "vol-";
     Serial.println(command);
   }
-  if (cmd.value == 0xFF906F) {
+  else if (cmd.value == 0xFF906F) {
     command = "up";
     Serial.println(command);
   }
-  if (cmd.value == 0xFF6897) {
+  else if (cmd.value == 0xFF6897) {
     command = "zero";
     Serial.println(command);
   }
-  if (cmd.value == 0xFF9867) {
+  else if (cmd.value == 0xFF9867) {
     command = "eq";
     Serial.println(command);
   }
-  if (cmd.value == 0xFFB04F) {
+  else if (cmd.value == 0xFFB04F) {
     command = "rept";
     Serial.println(command);
   }
-  if (cmd.value == 0xFF30CF) {
+  else if (cmd.value == 0xFF30CF) {
     command = "one";
     Serial.println(command);
   }
-  if (cmd.value == 0xFF18E7) {
+  else if (cmd.value == 0xFF18E7) {
     command = "two";
     Serial.println(command);
   }
-  if (cmd.value == 0xFF7A85) {
+  else if (cmd.value == 0xFF7A85) {
     command = "three";
     Serial.println(command);
   }
-  if (cmd.value == 0xFF10EF) {
+  else if (cmd.value == 0xFF10EF) {
     command = "four";
     Serial.println(command);
   }
-  if (cmd.value == 0xFF38C7) {
+  else if (cmd.value == 0xFF38C7) {
     command = "five";
     Serial.println(command);
   }
-  if (cmd.value == 0xFF5AA5) {
+  else if (cmd.value == 0xFF5AA5) {
     command = "six";
     Serial.println(command);
   }
-  if (cmd.value == 0xFF42BD) {
+  else if (cmd.value == 0xFF42BD) {
     command = "seven";
     Serial.println(command);
   }
-  if (cmd.value == 0xFF4AB5) {
+  else if (cmd.value == 0xFF4AB5) {
     command = "eight";
     Serial.println(command);
   }
-  if (cmd.value == 0xFF52AD) {
+  else if (cmd.value == 0xFF52AD) {
     command = "nine";
     Serial.println(command);
   }
+  else {
+    command = "null";
+    Serial.println(command);
+  }
+  
 
 }
