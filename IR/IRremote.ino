c0a010d0e2f25e4e3d7776bf86e1f8c4c90af22a
Nrxszv0 <58677365+Nrxszv0@users.noreply.github.com>
2020-06-23 21:08:12 -0500

    Update IRremote.ino
    Added more valued for each button. I was getting a lot of errors only using one value.

--- IR/IRremote.ino
+++ IR/IRremote.ino
@@ -1,4 +1,4 @@
-#include <IRremote.h>
+ #include <IRremote.h>
 int irPin = 10;
 IRrecv IR(irPin);
 decode_results cmd;
@@ -19,94 +19,75 @@ void loop() {
   Serial.print(cmd.value, HEX);
   Serial.print("==");
   IR.resume();
-  if (cmd.value == 0xFFA25D) {
-    command = "pwr";
-    Serial.println(command);
+  if (cmd.value == 0xFFA25D || cmd.value == 0xE318261B) {
+    command = "pwr";    
   }
-  else if (cmd.value == 0xFF629D) {
-    command = "vol+";
-    Serial.println(command);
+  else if (cmd.value == 0xFF629D || cmd.value ==0x511DBB) {
+    command = "vol+";    
   }
-  else if (cmd.value == 0xFFE21D) {
-    command = "stop";
-    Serial.println(command);
+  else if (cmd.value == 0xFFE21D || cmd.value ==0xEE886D7F) {
+    command = "stop";    
   }
-  else if (cmd.value == 0xFF22DD) {
-    command = "rew";
-    Serial.println(command);
+  else if (cmd.value == 0xFF22DD || cmd.value ==0x52A3D41F) {
+    command = "rew";    
   }
-  else if (cmd.value == 0xFF02FD) {
-    command = "pause";
-    Serial.println(command);
+  else if (cmd.value == 0xFF02FD || cmd.value ==0xD7E84B1B) {
+    command = "pause";    
   }
-  else if (cmd.value == 0xFFC23D) {
-    command = "FF";
-    Serial.println(command);
+  else if (cmd.value == 0xFFC23D || cmd.value ==0x20FE4DBB) {
+    command = "FF";    
   }
-  else if (cmd.value == 0xFFE01F) {
-    command = "down";
-    Serial.println(command);
+  else if (cmd.value == 0xFFE01F || cmd.value ==0xF076C13B) {
+    command = "down";    
   }
-  else if (cmd.value == 0xFFA857) {
-    command = "vol-";
-    Serial.println(command);
+  else if (cmd.value == 0xFFA857 || cmd.value ==0xA3C8EDDB) {
+    command = "vol-";    
   }
-  else if (cmd.value == 0xFF906F) {
-    command = "up";
-    Serial.println(command);
+  else if (cmd.value == 0xFF906F || cmd.value ==0xE5CFBD7F) {
+    command = "up";    
   }
-  else if (cmd.value == 0xFF6897) {
-    command = "zero";
-    Serial.println(command);
+  else if (cmd.value == 0xFF6897 || cmd.value ==0xC101E57B) {
+    command = "zero";    
   }
-  else if (cmd.value == 0xFF9867) {
-    command = "eq";
-    Serial.println(command);
+  else if (cmd.value == 0xFF9867 || cmd.value ==0x97483BFB) {
+    command = "eq";    
   }
-  else if (cmd.value == 0xFFB04F) {
-    command = "rept";
-    Serial.println(command);
+  else if (cmd.value == 0xFFB04F || cmd.value ==0xF0C41643) {
+    command = "rept";    
   }
-  else if (cmd.value == 0xFF30CF) {
-    command = "one";
-    Serial.println(command);
+  else if (cmd.value == 0xFF30CF || cmd.value ==0x9716BE3F) {
+    command = "one";    
   }
-  else if (cmd.value == 0xFF18E7) {
-    command = "two";
-    Serial.println(command);
+  else if (cmd.value == 0xFF18E7 || cmd.value ==0x3D9AE3F7) {
+    command = "two";    
   }
-  else if (cmd.value == 0xFF7A85) {
-    command = "three";
-    Serial.println(command);
+  else if (cmd.value == 0xFF7A85 || cmd.value ==0x6182021B) {
+    command = "three";    
   }
-  else if (cmd.value == 0xFF10EF) {
-    command = "four";
-    Serial.println(command);
+  else if (cmd.value == 0xFF10EF || cmd.value ==0x8C22657B) {
+    command = "four";    
   }
-  else if (cmd.value == 0xFF38C7) {
-    command = "five";
-    Serial.println(command);
+  else if (cmd.value == 0xFF38C7 || cmd.value ==0x488F3CBB) {
+    command = "five";    
   }
-  else if (cmd.value == 0xFF5AA5) {
-    command = "six";
-    Serial.println(command);
+  else if (cmd.value == 0xFF5AA5 || cmd.value ==0x449E79F) {
+    command = "six";    
   }
-  else if (cmd.value == 0xFF42BD) {
-    command = "seven";
-    Serial.println(command);
+  else if (cmd.value == 0xFF42BD || cmd.value ==0x32C6FDF7) {
+    command = "seven";    
   }
-  else if (cmd.value == 0xFF4AB5) {
-    command = "eight";
-    Serial.println(command);
+  else if (cmd.value == 0xFF4AB5 || cmd.value ==0x1BC0157B) {
+    command = "eight";    
   }
-  else if (cmd.value == 0xFF52AD) {
-    command = "nine";
-    Serial.println(command);
+  else if (cmd.value == 0xFF52AD || cmd.value ==0x3EC3FC1B) {
+    command = "nine";    
+  }
+  else if(cmd.value == 0x61A06897) {
+    command ="left";    
   }
   else {
-    command = "null";
-    Serial.println(command);
+    command = "null";    
   }
-  
+  Serial.println(command);
 
 }
